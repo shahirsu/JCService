63fa3563b72cc1525c0275559e573e46bd3c0149
Shahir Sudhir <sshahirs@amazon.com>
2023-07-11 15:10:29 +0200

    t

---
+++ src/main/java/com/amz/jcservice/Index.java
@@ -0,0 +1,12 @@
+package com.amz.jcservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class Index {
+
+    @GetMapping("/")
+    public ModelAndView AtoredPage() {
+        return new ModelAndView("index");
+    }
+}
