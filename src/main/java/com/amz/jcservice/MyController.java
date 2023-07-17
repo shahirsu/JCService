ea15fb461fbc6c50cdb5287c805deed4265d0793
Shahir Sudhir <sshahirs@amazon.com>
2023-07-17 16:20:20 +0200

    t

--- src/main/java/com/amz/jcservice/MyController.java
+++ src/main/java/com/amz/jcservice/MyController.java
@@ -5,6 +5,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
@@ -19,5 +20,18 @@ public class MyController {
         Resource resource = new ClassPathResource(filePath);
 
         return resource;
+    }
+    @GetMapping("/page")
+    public ModelAndView getPage1() {
+        // Create a new ModelAndView object
+        ModelAndView modelAndView = new ModelAndView();
+
+        // Set the view name to the JSP file path
+        modelAndView.setViewName("image_downloader/hello.jsp");
+
+        // Add any necessary data to the model
+        // modelAndView.addObject("attributeName", attributeValue);
+
+        return modelAndView;
     }
 }
