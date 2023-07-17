7417220dc8c250ab168045a39c43887e134c58d6
Shahir Sudhir <sshahirs@amazon.com>
2023-07-17 16:32:11 +0200

    t

--- src/main/java/com/amz/jcservice/MyController.java
+++ src/main/java/com/amz/jcservice/MyController.java
@@ -12,21 +12,6 @@ import java.util.Map;
 @Controller
 public class MyController {
 
-    private static final String CLIENT_ID_PARAM_NAME = "id";
-    private static final String WEB = "index.html";
-
-    @RequestMapping(value = {"/{id}/index"}, // Endpoint to handle GET requests for /{id}/index with id controlled by user
-            method = RequestMethod.GET)
-    // Create a model map to hold the "id" parameter. Parameter id is extracted from the path as a variable
-    public ModelAndView welcome(@PathVariable String id) {
-        Map modelMap = new HashMap<>();
-        modelMap.put(CLIENT_ID_PARAM_NAME, id);
-        ModelAndView modelAndView = new ModelAndView(WEB, modelMap); // Create a ModelAndView object with the "WEB" view and the model map
-        return modelAndView;
-
-
-    }
-
     @GetMapping("/")
     @ResponseBody
     public Resource getPage() {
