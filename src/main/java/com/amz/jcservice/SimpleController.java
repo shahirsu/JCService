1da4c94489ae11b8baa91c3dd3de71ce50da9e1a
Shahir Sudhir <sshahirs@amazon.com>
2023-07-17 16:51:10 +0200

    t

--- src/main/java/com/amz/jcservice/SimpleController.java
+++ src/main/java/com/amz/jcservice/SimpleController.java
@@ -1,12 +1,10 @@
 package com.amz.jcservice;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class SimpleController {
@@ -14,11 +12,10 @@ public class SimpleController {
     private static final String CLIENT_ID_PARAM_NAME = "id";
     private static final String WEB = "/image_downloader/index.html";
 
-    @RequestMapping(value = "/{id}/index", method = RequestMethod.GET)
-    public ModelAndView welcome(@PathVariable String id) {
-        Map<String, Object> modelMap = new HashMap<>();
-        modelMap.put(CLIENT_ID_PARAM_NAME, id);
-        ModelAndView modelAndView = new ModelAndView(WEB, modelMap);
-        return modelAndView;
+    @GetMapping("/{id}/index")
+    public String welcome(@PathVariable String id, Model model) {
+        model.addAttribute(CLIENT_ID_PARAM_NAME, id);
+        return WEB;
     }
 }
+
