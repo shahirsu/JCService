4907eebce43e0bbbc650da7befde1d553099aedb
Shahir Sudhir <sshahirs@amazon.com>
2023-07-17 16:35:14 +0200

    t

---
+++ src/main/java/com/amz/jcservice/SimpleController.java
@@ -0,0 +1,30 @@
+package com.amz.jcservice;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+
+public class SimpleController {
+
+    private static final String CLIENT_ID_PARAM_NAME = "id";
+    private static final String WEB = "image_downloader/index.html";
+
+    @RequestMapping(value = {"/{id}/index"}, // Endpoint to handle GET requests for /{id}/index with id controlled by user
+            method = RequestMethod.GET)
+    // Create a model map to hold the "id" parameter. Parameter id is extracted from the path as a variable
+    public ModelAndView welcome(@PathVariable String id) {
+        Map modelMap = new HashMap<>();
+        modelMap.put(CLIENT_ID_PARAM_NAME, id);
+        ModelAndView modelAndView = new ModelAndView(WEB, modelMap); // Create a ModelAndView object with the "WEB" view and the model map
+        return modelAndView;
+
+
+    }
+}
