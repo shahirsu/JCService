4fb4182cb690568c8f3e6a6d923b790336127bf2
Shahir Sudhir <sshahirs@amazon.com>
2023-07-17 17:32:18 +0200

    t

--- src/main/java/com/amz/jcservice/MyController.java
+++ src/main/java/com/amz/jcservice/MyController.java
@@ -5,19 +5,4 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-public class MyController {
-
-    @GetMapping("/")
-    @ResponseBody
-    public Resource getPage() {
-        // Path to the HTML file relative to the "resources" folder
-        String filePath = "templates/index.html";
-
-        // Load the HTML file as a resource
-        Resource resource = new ClassPathResource(filePath);
-
-        return resource;
-
-    }
-}
+//
