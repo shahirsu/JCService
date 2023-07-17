df9deacc6c63ac726cf79e77da151bfdf22d294c
Shahir Sudhir <sshahirs@amazon.com>
2023-07-17 17:26:01 +0200

    t

--- src/main/java/com/amz/jcservice/SimpleController.java
+++ src/main/java/com/amz/jcservice/SimpleController.java
@@ -1,20 +1,32 @@
 package com.amz.jcservice;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import java.util.HashMap;
+import java.util.Map;
+
 public class SimpleController {
 
     private static final String CLIENT_ID_PARAM_NAME = "id";
-    private static final String WEB = "/templates/index.html";
+    private static final String WEB = "index.html";
+    @RequestMapping(value = {"/{id}/index"}, // Endpoint to handle GET requests for /{id}/index with id controlled by user
+            method = RequestMethod.GET)
+    // Create a model map to hold the "id" parameter. Parameter id is extracted from the path as a variable
+    public ModelAndView welcome(@PathVariable String id) {
+        Map modelMap = new HashMap<>();
+        modelMap.put(CLIENT_ID_PARAM_NAME,id);
+        ModelAndView modelAndView = new ModelAndView(WEB,modelMap); // Create a ModelAndView object with the "WEB" view and the model map
+        return modelAndView;
 
-    @GetMapping("/{id}/index")
-    public String welcome(@PathVariable String id, Model model) {
-        model.addAttribute(CLIENT_ID_PARAM_NAME, id);
-        return WEB;
+
     }
-}
+    @GetMapping("/")
+    public ModelAndView AtoredPage() {
+        return new ModelAndView("index");
+    }
 
+}
