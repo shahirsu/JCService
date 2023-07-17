4b2aae0da915a0179954093aabe2036296ccb9ac
Shahir Sudhir <sshahirs@amazon.com>
2023-07-17 16:27:57 +0200

    t

--- src/main/java/com/amz/jcservice/MyController.java
+++ src/main/java/com/amz/jcservice/MyController.java
@@ -3,12 +3,29 @@ package com.amz.jcservice;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class MyController {
+
+    private static final String CLIENT_ID_PARAM_NAME = "id";
+    private static final String WEB = "index.html";
+
+    @RequestMapping(value = {"/{id}/index"}, // Endpoint to handle GET requests for /{id}/index with id controlled by user
+            method = RequestMethod.GET)
+    // Create a model map to hold the "id" parameter. Parameter id is extracted from the path as a variable
+    public ModelAndView welcome(@PathVariable String id) {
+        Map modelMap = new HashMap<>();
+        modelMap.put(CLIENT_ID_PARAM_NAME, id);
+        ModelAndView modelAndView = new ModelAndView(WEB, modelMap); // Create a ModelAndView object with the "WEB" view and the model map
+        return modelAndView;
+
+
+    }
 
     @GetMapping("/")
     @ResponseBody
@@ -20,18 +37,6 @@ public class MyController {
         Resource resource = new ClassPathResource(filePath);
 
         return resource;
-    }
-    @GetMapping("/page")
-    public ModelAndView getPage1() {
-        // Create a new ModelAndView object
-        ModelAndView modelAndView = new ModelAndView();
 
-        // Set the view name to the JSP file path
-        modelAndView.setViewName("image_downloader/hello.jsp");
-
-        // Add any necessary data to the model
-        // modelAndView.addObject("attributeName", attributeValue);
-
-        return modelAndView;
     }
 }
